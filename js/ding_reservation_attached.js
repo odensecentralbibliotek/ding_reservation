d3efeb8c4213cc2dbef77bb04e53a1108e22fc02
Tobias F. Petersen <tobsf@hotmail.com>
2016-02-04 15:26:00 +0100

    adding preloader to tabs

--- js/ding_reservation_attached.js
+++ js/ding_reservation_attached.js
@@ -8,7 +8,7 @@ jQuery( document ).ready(function() {
        jQuery('<div class="search-overlay--wrapper"><div class="search-overlay--inner"><i class="icon-spinner icon-spin search-overlay--icon"></i><p class="search-overlay--text">' + Drupal.t('Sletter reservationer...') + '</p><p class="cancel"><a href="#">' + Drupal.t('Luk') + '</a></p></div></div>').prependTo('body');
     });
     */
-    jQuery('.nav-tabs').on('click',function(){
+       jQuery('.nav-tabs').on('click',function(){
        jQuery('<div class="search-overlay--wrapper"><div class="search-overlay--inner"><i class="icon-spinner icon-spin search-overlay--icon"></i><p class="search-overlay--text">' + Drupal.t('Henter oversigt...') + '</p><p class="cancel"><a href="#">' + Drupal.t('Luk') + '</a></p></div></div>').prependTo('body');
     });
 });
